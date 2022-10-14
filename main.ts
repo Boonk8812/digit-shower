b87ec22568507d7059b221ed195a47c743e873b6
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2022-10-14 11:28:03 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+images.createImage(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `).showImage(0)
+images.createImage(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `).showImage(0)
+images.createImage(`
+    . # # # .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # # # .
+    `).showImage(0)
+basic.showString("7")
+basic.showString("3")
+basic.showString("7-5")
+basic.forever(function () {
+	
+})
