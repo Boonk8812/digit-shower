c596b3f50ad1b5b5883d2f01cd719c1be98a08d7
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2022-11-08 11:02:13 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,9 +9,24 @@ images.createImage(`
     . . # . .
     . . # . .
     . . # . .
-    . . # . .
+    . . . . .
     . . # . .
     `).showImage(0)
+images.createImage(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # # # .
+    `).showImage(0)
+images.createImage(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . # . . .
+    . # # # .
+    `).showImage(0)
+WiFiBit.connectToWiFiBit()
 images.createImage(`
     . # # # .
     . # . # .
@@ -19,9 +34,3 @@ images.createImage(`
     . # . # .
     . # # # .
     `).showImage(0)
-basic.showString("7")
-basic.showString("3")
-basic.showString("7-5")
-basic.forever(function () {
-	
-})
